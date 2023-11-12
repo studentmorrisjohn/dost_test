3b1f25d0af92f9f2e1bb2c5ea8ec338f6a01904a
Morris Pogi <mjamontemayor@iskolarngbayan.pup.edu.ph>
2023-11-12 21:38:34 +0800

    make predictions api

--- app.py
+++ app.py
@@ -2,7 +2,58 @@ from flask import Flask, request, render_template, jsonify
 from PIL import Image
 from io import BytesIO
 
+import tensorflow as tf
+import cv2 
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+
 app = Flask(__name__)
+
+
+def make_prediction(imgage):
+
+    # Load the model
+    new_model = tf.keras.models.load_model("Final_model.h5")
+
+
+    # Reading the image 
+    img = cv2.cvtColor(np.array(imgage), cv2.COLOR_RGB2BGR)
+
+    # Converting image to grayscale 
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+
+    # Loading the required haar-cascade xml classifier file 
+    haar_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'Haarcascade_frontalface_default.xml') 
+    
+    # Applying the face detection method on the grayscale image 
+    faces_rect = haar_cascade.detectMultiScale(gray_img, 1.1, 9)
+    
+    # Zooming in on face
+    for x,y,w,h in faces_rect:
+        roi_gray = gray_img[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
+        facess = haar_cascade.detectMultiScale(roi_gray)
+        if len(facess) == 0:
+            print("Face not detected")
+        else:
+            for(ex,ey,ew,eh) in facess:
+                face_roi = roi_color[ey: ey+eh, ex: ex+ew]
+            
+    # Resizing image before making a prediction
+    final_image = cv2.resize(face_roi, (224,224))
+    final_image = np.expand_dims(final_image, axis=0)
+    final_image = final_image/255.0
+    
+    # Making the Prediction
+    Predictions = new_model.predict(final_image)
+    
+    for (x, y, w, h) in faces_rect: 
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2) 
+    
+    
+    return img, np.argmax(Predictions)
 
 @app.route('/howItWorks')
 def how_it_works():
@@ -32,9 +83,10 @@ def process_image():
     try:
         # Read the image file
         img = Image.open(file)
+        image, prediction = make_prediction(img)
 
         # Process the image (you can replace this with your own logic)
-        result_string = f"Image processed. Dimensions: {img.size}"
+        result_string = f"Image processed. prediction: {prediction}"
 
         return jsonify({'result': result_string})
 
